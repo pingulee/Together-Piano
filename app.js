f5682a19f85d31ddb2185da59c752a51ff6270a8
이민구 <escapeweedy@gmail.com>
2023-10-22 17:57:45 +0900

    refactor: if를 switch case로 변경

--- app.js
+++ app.js
@@ -15,55 +15,75 @@ const db = require('./public/javascripts/db');
 //   }
 // });
 
+
+
 const server = http.createServer((request, response) => {
-  if (request.method === 'GET' && request.url === '/') {
-    response.writeHead(200, ContentType.html);
-    response.end(fs.readFileSync('./public/index.html', 'utf8'));
-  }
+  switch (request.method) {
+    case 'GET':
+      if (request.url === '/') {
+        response.writeHead(200, ContentType.html);
+        response.end(fs.readFileSync('./public/index.html', 'utf8'));
+      }
+      else if (request.url === '/stylesheets/style.css') {
+        response.writeHead(200, ContentType.css);
+        response.end(fs.readFileSync('./public/stylesheets/style.css', 'utf8'));
+      }
+      else if (request.url === '/javascripts/script.js') {
+        response.writeHead(200, ContentType.js);
+        response.end(fs.readFileSync('./public/javascripts/script.js', 'utf8'));
+      }
+      break;
 
-  else if (request.method === 'GET' && request.url === '/stylesheets/style.css') {
-    response.writeHead(200, ContentType.css);
-    response.end(fs.readFileSync('./public/stylesheets/style.css', 'utf8'));
-  }
+    case 'POST':
+      if (request.url === '/javascripts/script.js') {
+        response.writeHead(200, ContentType.js);
+        response.end(fs.readFileSync('./public/javascripts/script.js', 'utf8'));
+      }
+      else if (request.url === '/javascripts/script.js') {
+        response.writeHead(200, ContentType.js);
+        response.end(fs.readFileSync('./public/javascripts/script.js', 'utf8'));
+      }
+      else if (request.url === '/join') {
+        response.writeHead(200, ContentType.html);
+        response.end(fs.readFileSync('./public/join.html', 'utf8'));
+      }
+      break;
 
-  else if (request.method === 'GET' && request.url === '/javascripts/script.js') {
-    response.writeHead(200, ContentType.js);
-    response.end(fs.readFileSync('./public/javascripts/script.js', 'utf8'));
-  }
-
-  else if (request.method === 'POST' && request.url === '/join') {
-    response.writeHead(200, ContentType.html);
-    response.end(fs.readFileSync('./public/join.html', 'utf8'));
-  }
-
-  // else if (request.method === 'POST' && request.url === '/join') {
-  //   let body = "";
-
-  //   request.on('data', (chunk) => {
-  //     body += chunk.toString();
-  //   });
-  //   request.on('end', () => {
-  //     const { id, pw1, pw2, email } = querystring.parse(body);
-  //     const data = db.one + id + db.two;
-  //     fs.writeFileSync('./public/success.html', data)
-  //     if (validation(id, pw1, pw2, email)) {
-  //       signUpAsset.id = id;
-  //       signUpAsset.pw = pw1;
-  //       signUpAsset.email = email;
-  //       response.writeHead(200, ContentType.html);
-  //       response.end(fs.readFileSync('./public/success.html', 'utf8'));
-  //     } else {
-  //       response.end(fs.readFileSync('./public/index.html', 'utf8'));
-  //     }
-  //   });
-  // }
-
-  else {
-    response.writeHead(404, ContentType.html);
-    response.end('404 ERROR');
+    default:
+      response.writeHead(404, ContentType.html);
+      response.end('404 ERROR');
+      break;
   }
 });
 
 server.listen(port, () => {
   console.log('서버 가동 중 : http://localhost:8080/');
 });
+
+
+
+
+// else if (request.method === 'POST' && request.url === '/join') {
+//   let body = "";
+
+//   request.on('data', (chunk) => {
+//     body += chunk.toString();
+//   });
+//   request.on('end', () => {
+//     const { id, pw1, pw2, email } = querystring.parse(body);
+//     const data = db.one + id + db.two;
+//     fs.writeFileSync('./public/success.html', data)
+//     if (validation(id, pw1, pw2, email)) {
+//       signUpAsset.id = id;
+//       signUpAsset.pw = pw1;
+//       signUpAsset.email = email;
+//       response.writeHead(200, ContentType.html);
+//       response.end(fs.readFileSync('./public/success.html', 'utf8'));
+//     } else {
+//       response.end(fs.readFileSync('./public/index.html', 'utf8'));
+//     }
+//   });
+// }
+
+
+
