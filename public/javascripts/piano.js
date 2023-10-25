c1525977fd1c817ae027aa14bcb255d12b42bcde
leemingoo <dwmapi@naver.com>
2023-10-25 12:44:51 +0900

    style: 피아노 클릭 시 색상 변경

--- public/javascripts/piano.js
+++ public/javascripts/piano.js
@@ -1,28 +1,24 @@
-const keys = document.querySelectorAll('.key');
-const c5 = document.getElementById('c5');
+const pianoKeys = document.querySelectorAll('.key');
 
-const sustainPedal = document.getElementById('sustain-pedal');
-let isSustainOn = false;
+  // 피아노 키를 클릭했을 때의 이벤트 핸들러를 등록합니다.
+  pianoKeys.forEach(function (key) {
+    key.addEventListener('mousedown', function () {
+      // 클릭한 피아노 키의 data-note 속성 값을 가져옵니다.
+      const note = key.getAttribute('data-note');
+      
+      // 해당하는 오디오 엘리먼트를 찾아서 재생합니다.
+      const audioElement = document.getElementById(note);
+      if (audioElement) {
+        audioElement.currentTime = 0; // 오디오를 처음으로 되감습니다.
+        audioElement.play(); // 오디오 재생
+      }
 
-keys.forEach(key => {
-  const _note = key.getAttribute('data-note');
-  key.addEventListener('mousedown', () => {
-    console.log(note);
-    _note.play();
+      // 배경 색상 변경
+      key.style.backgroundColor = 'lightgray'; // 변경할 배경 색상을 지정합니다.
+    });
+
+    key.addEventListener('mouseup', function () {
+      // 마우스를 뗐을 때 배경 색상을 초기화합니다.
+      key.style.backgroundColor = '';
+    });
   });
-
-  key.addEventListener('mouseup', () => {
-    _note.pause();
-    _note.currentTime = 0;
-  });
-
-  key.addEventListener('mouseout', () => {
-    _note.pause();
-    _note.currentTime = 0;
-  });
-
-  key.style.backgroundColor = 'green';
-  setTimeout(() => {
-    key.style.backgroundColor = key.classList.contains('white') ? 'white' : 'black';
-  }, 100);
-});
