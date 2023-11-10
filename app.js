10808198c6fd5ba31cbaac6c94ab37a7fdccc5d5
leemingoo <dwmapi@naver.com>
2023-11-10 17:50:17 +0900

    refactor: 디렉토리 구조 변경

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const routes = require('./routes/routes');
+const routes = require('./routes');
 
 const port = 8080;
 
@@ -10,4 +10,4 @@ app.use('/', routes);
 
 server.listen(port, () => {
   console.log(`http://localhost:8080`);
-}); 
+});
