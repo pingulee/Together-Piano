da3d30f98b036032439528c9cabc58746c51721e
Pingu Lee <escapeweedy@gmail.com>
2023-11-14 17:25:09 +0900

    feat: 피아노 레이아웃 간단 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import './styles/App.css';
-function App() {
+import React from 'react';
+import PianoKey from './components/PianoKey';
+
+const App: React.FC = () => {
   return (
     <div>
-      hello
+      <PianoKey note="C" isBlack={false} />
+      <PianoKey note="Db" isBlack={true} />
+      <PianoKey note="D" isBlack={false} />
+      <PianoKey note="Eb" isBlack={true} />
+      <PianoKey note="E" isBlack={false} />
+      <PianoKey note="F" isBlack={false} />
+      <PianoKey note="Gb" isBlack={true} />
+      <PianoKey note="G" isBlack={false} />
+      <PianoKey note="Ab" isBlack={true} />
+      <PianoKey note="A" isBlack={false} />
+      <PianoKey note="Bb" isBlack={true} />
+      <PianoKey note="B" isBlack={false} />
     </div>
   );
-}
+};
 
 export default App;
