cba4757cc106038232cd4d149e18731e0c9b8c94
Pingu Lee <escapeweedy@gmail.com>
2023-11-14 11:29:56 +0900

    feat: react 기본적인 구조

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,5 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
