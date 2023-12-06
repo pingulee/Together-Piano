3e161abb67f0f4f4576e7ad881da7d0523502b6f
이민구 <escapeweedy@gmail.com>
2023-12-06 11:53:39 +0900

    feat: CRA를 통한 개발 환경 구축

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './components/App/App'
+import ReactDOM from 'react-dom';
+import App from './components/App/App';
 
-const root = createRoot(document.getElementById('root') as HTMLElement);   
-root.render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
