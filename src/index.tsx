1524bb1229732c09c600144ff9012d50820b2496
이민구 <escapeweedy@gmail.com>
2023-12-06 12:05:18 +0900

    feat: createRoot 사용

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App/App';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
