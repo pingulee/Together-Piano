75702d527cee09fb66cf6f95ea9a4d337d295cf3
이민구 <escapeweedy@gmail.com>
2023-12-19 12:35:28 +0900

    feat: 바닐라 자바스크립트 스타일링

---
+++ script.js
@@ -0,0 +1,13 @@
+let piano = document.getElementsByClassName("piano");
+const base = "./audio/";
+window.onload = () => {
+  for (let index = 1; index <= 88; index++) {
+    let div = document.createElement("div");
+    div.classList.add("key", index <= 34 ? "black-key" : "white-key");
+    const number = index <= 9 ? "0" + index : index;
+    div.addEventListener("click", () => {
+      new Audio(`${base}key${number}.mp3`).play();
+    });
+    piano[0].appendChild(div);
+  }
+};
