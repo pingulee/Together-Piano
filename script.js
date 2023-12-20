a6193e5d35a7af28b5aca4edf4d5dc795f55e9a0
이민구 <escapeweedy@gmail.com>
2023-12-20 18:51:45 +0900

    하..

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const base = "./audio/";
 window.onload = () => {
   for (let index = 1; index <= 88; index++) {
     let div = document.createElement("div");
-    div.classList.add("key", index <= 34 ? "black-key" : "white-key");
+    div.classList.add("key", index <= 36 ? "black-key" : "white-key");
     const number = index <= 9 ? "0" + index : index;
     div.addEventListener("click", () => {
       new Audio(`${base}key${number}.mp3`).play();
     });
     piano[0].appendChild(div);
   }
-};
+};  
