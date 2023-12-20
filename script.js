615bf9ece1c1f9d5363f9ada3a676adafe6521ee
이민구 <escapeweedy@gmail.com>
2023-12-20 19:59:37 +0900

    feat: 검은건반 동적 위치 조정 시작

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-let piano = document.getElementsByClassName("piano-keyboard");
+let piano = document.getElementById("piano-keyboard");
+
+
+let pianoWidth = piano.clientWidth;
+let pianoHeight = piano.clientHeight;
+
 const base = "./audio/";
-window.onload = () => {
-  for (let index = 1; index <= 88; index++) {
-    let div = document.createElement("div");
-    div.classList.add("key", index <= 36 ? "black-key" : "white-key");
-    const number = index <= 9 ? "0" + index : index;
-    div.addEventListener("click", () => {
-      new Audio(`${base}key${number}.mp3`).play();
-    });
-    piano[0].appendChild(div);
-  }
-};  
+
+for (let index = 1; index <= 88; index++) {
+  let div = document.createElement("div");
+  div.classList.add("key", index <= 36 ? "black-key" : "white-key");
+  const number = index <= 9 ? "0" + index : index;
+  div.addEventListener("click", () => {
+    new Audio(`${base}key${number}.mp3`).play();
+  });
+  piano.appendChild(div);
+}
