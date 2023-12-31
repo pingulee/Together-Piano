80e044afbfa28f5ff7b7f987e71e5afc5669a8a9
이민구 <escapeweedy@gmail.com>
2023-12-31 22:11:44 +0900

    feat: 불필요 파일 삭제

--- app/components/Footer/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className='bg-gray-800 text-white text-center py-4'>
-      <p>&copy; 2023 내 화려한 블로그. All rights reserved.</p>
-    </footer>
-  );
-}
