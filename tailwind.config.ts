9d4c2a16ed60016727f57caf327240e3d36c29cc
이민구 <escapeweedy@gmail.com>
2023-12-31 21:23:40 +0900

    feat: tailwind 커스텀 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundColor: {
+        main: '#222020',
+        sub: '#2a2828',
+      },
+      colors: {
+        highlight: '#d74242',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
