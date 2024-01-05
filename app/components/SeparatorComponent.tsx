92db4eb795455e7bc6dcf08b9d1bd7f054bac068
이민구 <escapeweedy@gmail.com>
2024-01-06 07:01:12 +0900

    feat: 색상 변환

--- app/components/SeparatorComponent.tsx
+++ app/components/SeparatorComponent.tsx
@@ -1,7 +1,7 @@
 export default function SeparatorComponent() {
   return (
     <div className='pt-2'>
-      <div className='h-1 bg-gray-600 rounded-sm flex'></div>
+      <div className='h-0.5 bg-red-200 rounded-sm flex'></div>
     </div>
   );
 }
