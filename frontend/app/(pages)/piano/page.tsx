a77fd56bc203e3ec79c86a07bb52b19f773e22fc
이민구 <escapeweedy@gmail.com>
2024-01-09 21:47:46 +0900

    feat: 머지를 위한 임시 백업

--- frontend/app/(pages)/piano/page.tsx
+++ frontend/app/(pages)/piano/page.tsx
@@ -1,14 +1,40 @@
 'use client';
 import ChatComponent from '@/app/components/Chat/ChatComponent';
 
-import './test.css';
+type PianoKey = {
+  color: 'white' | 'black';
+  keyNumber: number;
+};
 
-export default function PianoPage() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <Piano />
-      </header>
-    </div>
-  );
+export default function PianoPage(): string {
+  let keys: PianoKey[] = [];
+  let keyNumber = 1;
+  let isBlackKeyNext = true;
+
+  while (keyNumber <= 88) {
+    keys.push({ color: 'white', keyNumber: keyNumber++ });
+
+    // Determine if a black key should be added next
+    if (
+      isBlackKeyNext &&
+      keyNumber % 88 !== 0 &&
+      keyNumber % 88 !== 3 &&
+      keyNumber % 88 !== 4 &&
+      keyNumber % 88 !== 7 &&
+      keyNumber % 88 !== 8 &&
+      keyNumber % 88 !== 11
+    ) {
+      keys.push({ color: 'black', keyNumber: keyNumber++ });
+    }
+
+    // Toggle the flag for the next black key based on the pattern
+    isBlackKeyNext =
+      !isBlackKeyNext || keyNumber % 88 === 4 || keyNumber % 88 === 11;
+  }
+
+  return keys
+    .map(
+      (key) => `<li className='${key.color}' data-key='${key.keyNumber}'></li>`,
+    )
+    .join('\n');
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className='flex justify-center items-center h-screen w-screen'>
+    <div className='flex justify-center items-center h-screen w-full'>
       test
     </div>
   );
