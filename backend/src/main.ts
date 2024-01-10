0eb4df1e82bc026807dabb3209928ca2b1fc4e33
이민구 <escapeweedy@gmail.com>
2024-01-10 19:15:15 +0900

    feat: nest 설치 완료

---
+++ backend/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors(); // CORS 활성화
+  await app.listen(3000);
+}
+bootstrap();
