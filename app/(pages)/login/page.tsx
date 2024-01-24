2048e31470603e367fbb94f6e14aa91d8bb7f870
이민구 <escapeweedy@gmail.com>
2024-01-24 12:16:12 +0900

    feat: 디자인 작업을 위한 aws-amplify 설치 + 구글 로그인 완성

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -7,20 +7,9 @@ export default function SettingPage() {
   const { data: session } = useSession();
   const router = useRouter();
 
-  useEffect(() => {
-    if (session) {
-      console.log('이미 로그인 상태');
-      router.push('/'); // '/' 경로로 리디렉션
-    }
-  }, [session, router]);
-
   return (
     <div>
-      {session ? (
-        <button onClick={() => signOut()}>로그아웃</button>
-      ) : (
-        <button onClick={() => signIn()}>로그인</button>
-      )}
+      <button onClick={() => signIn('google', { callbackUrl: '/' })}>gg</button>
     </div>
   );
 }
