022be8fdd754931958f6eb604d47bb58933ee455
이민구 <escapeweedy@gmail.com>
2024-01-25 21:48:17 +0900

    feat: cors 설치

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,44 @@ import { io } from 'socket.io-client';
 const socket = io('http://localhost:3288');
 
 export default function Home() {
+  const [message, setMessage] = useState('');
+  const [receivedMessages, setReceivedMessages] = useState<string[]>([]);
+
   useEffect(() => {
-    socket.on('connect', () => {
-      console.log('connected to server');
+    // 서버로부터 메시지를 받을 때의 이벤트 리스너
+    socket.on('message', (newMessage: string) => {
+      setReceivedMessages((prevMessages) => [...prevMessages, newMessage]);
     });
 
-    socket.on('message', (data) => {
-      console.log('Message from server:', data);
-    });
-
+    // 컴포넌트가 언마운트될 때 소켓 연결 해제
     return () => {
-      socket.off('connect');
       socket.off('message');
-      socket.disconnect();
     };
   }, []);
+
+  const sendMessage = () => {
+    if (message) {
+      // 메시지 전송
+      socket.emit('message', message);
+      setMessage('');
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type='text'
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder='메시지를 입력하세요'
+      />
+      <button onClick={sendMessage}>전송</button>
+      <div>
+        <h3>받은 메시지:</h3>
+        {receivedMessages.map((msg, index) => (
+          <p key={index}>{msg}</p>
+        ))}
+      </div>
+    </div>
+  );
 }
