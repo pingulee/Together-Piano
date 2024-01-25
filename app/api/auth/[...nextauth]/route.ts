54030385664d008adc580ff64e32eb4ac0040d57
이민구 <escapeweedy@gmail.com>
2024-01-25 16:22:21 +0900

    feat: git ignore 추가

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,16 +4,10 @@ import GoogleProvider from 'next-auth/providers/google';
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.REACT_APP_GOOGLE_CLIENT_SECRET || '',
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET ?? '',
     }),
   ],
-  callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      console.log(user.id);
-      return true;
-    },
-  },
 });
 
 export { handler as GET, handler as POST };
