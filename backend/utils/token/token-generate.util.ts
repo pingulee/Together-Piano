a2bf96cb7246439fc76e18b1dec0898b671ec9c5
이민구 <escapeweedy@gmail.com>
2024-01-26 20:40:26 +0900

    feat: tsconfing 작성

---
+++ backend/utils/token/token-generate.util.ts
@@ -0,0 +1,10 @@
+export default function tokenGenerate(tokenLength = 10) {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let token = '';
+  for (let i = 0; i < tokenLength; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    token += characters.charAt(randomIndex);
+  }
+  return token;
+}
