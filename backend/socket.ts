f003caf481a6ab362b582c90f3116c8cb6605b7e
이민구 <escapeweedy@gmail.com>
2024-01-26 11:38:22 +0900

    feat: 백엔드와 통합

---
+++ backend/socket.ts
@@ -0,0 +1,24 @@
+import http from 'http';
+import { Server } from 'socket.io';
+
+let interval: number = 1000;
+
+const socket = (server: http.Server) => {
+  const io = new Server(server, {
+    cors: {
+      origin: '*',
+    },
+  });
+
+  io.on('connect', (socket) => {
+    console.log('사용자 연결', socket.id);
+
+    socket.on('disconnect', () => console.log('사용자 연결 끊김', socket.id));
+
+    socket.on('message', (data: any) => {
+      console.log(data); // 클라이언트 -> 서버
+    });
+  });
+};
+
+export default socket;
