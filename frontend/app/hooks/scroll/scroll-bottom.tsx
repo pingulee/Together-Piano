0c64982ceb36727e9f8a7e62cbb7ca3ac54fb086
이민구 <escapeweedy@gmail.com>
2024-01-28 18:57:51 +0900

    feat: 수정

--- frontend/app/hooks/scroll/scroll-bottom.tsx
+++ frontend/app/hooks/scroll/scroll-bottom.tsx
@@ -5,7 +5,7 @@ export const useAutoScrollToBottom = (dependency: any[]) => {
 
   useEffect(() => {
     bottomRef.current?.scrollIntoView();
-  }, [...dependency]); // 뭘까?
+  }, [dependency]); // 뭘까?
 
   return bottomRef;
 };
