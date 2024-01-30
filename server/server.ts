a74c6af9171944a167790f91eae2355d4c9b739b
Pingu Lee <escapeweedy@gmail.com>
2024-01-30 12:47:48 +0900

    feat: next, express 통합

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,21 @@
-import http from 'http';
+import express from 'express';
 import { Server } from 'socket.io';
+import http, { createServer } from 'http';
+import next from 'next';
 
-const socket = (server: http.Server) => {
+const port = 3288;
+const dev = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev, port });
+const nextHandler = nextApp.getRequestHandler();
+
+nextApp.prepare().then(() => {
+  const app = express();
+  const server = createServer(app);
+
   const io = new Server(server, {
     cors: {
       origin: '*',
+      methods: ['GET', 'POST'],
     },
   });
 
@@ -30,7 +41,13 @@ const socket = (server: http.Server) => {
       socket.broadcast.emit('mouseMove', data);
       console.log(data);
     });
-  });
-};
 
-export default socket;
+    app.all('*', (req, res) => {
+      return nextHandler(req, res);
+    });
+
+    server.listen(port, () => {
+      console.log(`port : ${port}`);
+    });
+  });
+});
