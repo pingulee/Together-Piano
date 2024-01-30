bdf2bc3b423270a371e8bb090b24f00618eff7fc
Pingu Lee <escapeweedy@gmail.com>
2024-01-30 18:28:23 +0900

    feat: 완벽하게 완료

--- app/components/piano/piano-key.component.tsx
+++ app/components/piano/piano-key.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 interface PianoKeyProps {
   className: string;
@@ -6,19 +6,33 @@ interface PianoKeyProps {
 }
 
 export default function PianoKey({ className, note }: PianoKeyProps) {
-  // 마우스를 누를 때마다 새로운 Audio 객체를 생성하고 재생
+  const audioRef = useRef<HTMLAudioElement | null>(null); // 현재 재생 중인 Audio 객체를 저장할 ref
+
   const handleMouseDown = () => {
+    // 새 Audio 객체 생성 및 재생
     const audio = new Audio(`/sounds/${note}.mp3`);
-    audio.play().catch((error) => console.error('음악 재생 중 오류 발생:', error));
+    audio
+      .play()
+      .catch((error) => console.error('음악 재생 중 오류 발생:', error));
+    audioRef.current = audio; // 현재 재생 중인 Audio 객체를 ref에 저장
   };
 
-  // 마우스를 뗄 때 특별한 처리가 필요 없음
-  // 각각의 Audio 객체는 독립적으로 재생되며, 사용자가 마우스를 떼면 재생이 자연스럽게 끝남
+  const handleMouseUp = () => {
+    // ref에 저장된 Audio 객체의 재생을 중지
+    const audio = audioRef.current;
+    if (audio) {
+      audio.pause();
+      audio.currentTime = 0; // 오디오를 처음부터 다시 시작할 수 있도록 현재 시간을 0으로 설정
+      audioRef.current = null; // 재생이 끝났으므로 ref를 비움
+    }
+  };
 
   return (
     <div
       className={className}
       onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp} // 마우스가 키를 벗어날 때도 중지 처리
     />
   );
 }
