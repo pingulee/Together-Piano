3b3b0096fcfadc07fd3f6d8dc5d0effa50927558
Pingu Lee <escapeweedy@gmail.com>
2024-02-02 17:40:15 +0900

    feat: 타입 폴더 만들기

--- app/lib/database.ts
+++ app/lib/database.ts
@@ -11,7 +11,7 @@ if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-async function dbConnect() {
+export async function connectDatabase() {
   if (cached.conn) {
     return cached.conn;
   }
@@ -28,13 +28,3 @@ async function dbConnect() {
   cached.conn = await cached.promise;
   return cached.conn;
 }
-
-export const connectDatabase = async () => {
-  try {
-    await mongoose.connect(uri);
-    console.log('데이터베이스 연결 성공');
-  } catch (error) {
-    console.error('데이터베이스 연결 실패:', error);
-    process.exit(1);
-  }
-};
---
+++ types/next-auth.d.ts
@@ -0,0 +1,15 @@
+import 'next-auth';
+
+declare module 'next-auth' {
+  /**
+   * NextAuth의 `session` 객체에 사용자 정의 속성을 추가하기 위해 `Session` 인터페이스를 확장합니다.
+   */
+  interface Session {
+    user: {
+      id?: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
+}
